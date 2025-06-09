d40bf5bed395dac45c94493f5a1f03af4171f6ea
denyskozak <denys.kozak.self@gmail.com>
2025-06-09 09:04:57 +0100

    add ice-veins icon, improve character

--- client/next-js/components/parts/SkillBar.jsx
+++ client/next-js/components/parts/SkillBar.jsx
@@ -16,6 +16,10 @@ export const SkillBar = () => {
                 <div className="skill-icon" style={{backgroundImage: "url('/icons/shield.png')"}}></div>
                 <div className="skill-key" id="icebolt-cooldown">Q</div>
             </div>
+            <div className="skill-button">
+                <div className="skill-icon" style={{backgroundImage: "url('/icons/spell_veins.jpg')"}}></div>
+                <div className="skill-key" id="ice-veins">F</div>
+            </div>
             {/*<div className="skill-button">*/}
             {/*    <div className="skill-icon" style={{backgroundImage: "url('/icons/spell_fire_sealoffire.jpg')"}}></div>*/}
             {/*    <div className="skill-key" id="heal-cooldown">F</div>*/}
