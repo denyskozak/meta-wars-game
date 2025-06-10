bcd84fa858e991466bb92515d6a0a6b7c75f6f9b
Denys Kozak <117740720+denyskozak@users.noreply.github.com>
2025-06-10 07:45:44 +0100

    Require Immolate debuff for Conflagrate

---
+++ client/next-js/skills/warlock/immolate.js
@@ -0,0 +1,16 @@
+export const meta = { id: 'immolate', key: 'Q', icon: '/icons/spell_immolation.jpg' };
+
+export default function castImmolate({ playerId, globalSkillCooldown, isCasting, mana, getTargetPlayer, dispatch, sendToSocket, activateGlobalCooldown, startSkillCooldown }) {
+  if (globalSkillCooldown || isCasting) return;
+  if (mana < 30) {
+    console.log('Not enough mana for immolate!');
+    return;
+  }
+  const targetId = getTargetPlayer();
+  if (!targetId) {
+    dispatch({ type: 'SEND_CHAT_MESSAGE', payload: `No target for immolate!` });
+  }
+  sendToSocket({ type: 'CAST_SPELL', payload: { type: 'immolate', targetId } });
+  activateGlobalCooldown();
+  startSkillCooldown('immolate');
+}
