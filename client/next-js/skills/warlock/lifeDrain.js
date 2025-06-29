64425e43b0664d7b71fcd775196fa0f60182cb52
Denys Kozak <117740720+denyskozak@users.noreply.github.com>
2025-06-29 11:33:15 +0100

    fix(warlock): require target for life drain at cast end

--- client/next-js/skills/warlock/lifeDrain.js
+++ client/next-js/skills/warlock/lifeDrain.js
@@ -2,7 +2,15 @@ import { SPELL_COST } from '../../consts';
 
 export const meta = { id: 'lifedrain', key: '2', icon: '/icons/classes/warlock/lifedrain.jpg' };
 
-export default function castLifeDrain({ playerId, castSpellImpl, mana, getTargetPlayer, dispatch, sendToSocket, sounds }) {
+export default function castLifeDrain({
+  playerId,
+  castSpellImpl,
+  mana,
+  getTargetPlayer,
+  dispatch,
+  sendToSocket,
+  sounds,
+}) {
   if (mana < SPELL_COST['lifedrain']) {
     if (sounds?.noMana) {
       sounds.noMana.currentTime = 0;
@@ -11,17 +19,20 @@ export default function castLifeDrain({ playerId, castSpellImpl, mana, getTarget
     }
     return;
   }
-  const targetId = getTargetPlayer();
-  if (!targetId) {
-    dispatch({ type: 'SEND_CHAT_MESSAGE', payload: `No target for lifedrain!` });
-    sounds?.noTarget?.play?.();
-    return;
-  }
+
   castSpellImpl(
     playerId,
     SPELL_COST['lifedrain'],
     1000,
-    () => sendToSocket({ type: 'CAST_SPELL', payload: { type: 'lifedrain', targetId } }),
+    () => {
+      const targetId = getTargetPlayer();
+      if (!targetId) {
+        dispatch({ type: 'SEND_CHAT_MESSAGE', payload: `No target for lifedrain!` });
+        sounds?.noTarget?.play?.();
+        return;
+      }
+      sendToSocket({ type: 'CAST_SPELL', payload: { type: 'lifedrain', targetId } });
+    },
     sounds.spellCast,
     sounds.spellCast,
     meta.id,
