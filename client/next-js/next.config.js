e9ac74caee048b7fb495923572e64d33c006e9ac
denyskozak <denys.kozak.self@gmail.com>
2025-07-04 09:55:45 +0100

    fix war icons

--- client/next-js/next.config.js
+++ client/next-js/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: 'metawars.s3.eu-west-2.amazonaws.com',
+            port: '',
+            pathname: '/**'
+        }]
+    },
 };
 
 module.exports = nextConfig;
