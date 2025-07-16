3f7cc579a701a7ce0611492fa208de85b5349a23
denyskozak <denys.kozak.self@gmail.com>
2025-07-16 15:31:30 +0100

    tmp

--- client/next-js/components/game.jsx
+++ client/next-js/components/game.jsx
@@ -779,34 +779,34 @@ export function Game({models, sounds, textures, matchId, character}) {
         }
 
         // Lighting setup
-        const fillLight1 = new THREE.HemisphereLight(0x8dc1de, 0x00668d, 1.5);
+        const fillLight1 = new THREE.HemisphereLight(0x8dc1de, 0x00668d, 5);
         fillLight1.position.set(2, 1, 1);
         scene.add(fillLight1);
 
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 2.5);
-        directionalLight.position.set(-5, 25, -1);
-        directionalLight.castShadow = true;
-        directionalLight.shadow.camera.near = 0.01;
-        directionalLight.shadow.camera.far = 500;
-        directionalLight.shadow.camera.right = 30;
-        directionalLight.shadow.camera.left = -30;
-        directionalLight.shadow.camera.top = 30;
-        directionalLight.shadow.camera.bottom = -30;
-        directionalLight.shadow.mapSize.width = 1024;
-        directionalLight.shadow.mapSize.height = 1024;
-        directionalLight.shadow.radius = 4;
-        directionalLight.shadow.bias = -0.00006;
-        scene.add(directionalLight);
+        // const directionalLight = new THREE.DirectionalLight(0xffffff, 2.5);
+        // directionalLight.position.set(-5, 25, -1);
+        // directionalLight.castShadow = true;
+        // directionalLight.shadow.camera.near = 0.01;
+        // directionalLight.shadow.camera.far = 500;
+        // directionalLight.shadow.camera.right = 30;
+        // directionalLight.shadow.camera.left = -30;
+        // directionalLight.shadow.camera.top = 30;
+        // directionalLight.shadow.camera.bottom = -30;
+        // directionalLight.shadow.mapSize.width = 1024;
+        // directionalLight.shadow.mapSize.height = 1024;
+        // directionalLight.shadow.radius = 4;
+        // directionalLight.shadow.bias = -0.00006;
+        // scene.add(directionalLight);
 
-        const moonLight = new THREE.DirectionalLight(0x88aaff, 0.8);
-        moonLight.position.set(10, 20, -10);     // направление «луны»
-        moonLight.castShadow = true;             // тени от объектов
-        scene.add(moonLight);
+        // const moonLight = new THREE.DirectionalLight(0x88aaff, 0.8);
+        // moonLight.position.set(10, 20, -10);     // направление «луны»
+        // moonLight.castShadow = true;             // тени от объектов
+        // scene.add(moonLight);
 
-        const hemi = new THREE.HemisphereLight(0x111133, 0x000000, 0.6); // слабый «ночной» свет
-        scene.add(hemi);
-        const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
-        scene.add(ambientLight);
+        // const hemi = new THREE.HemisphereLight(0x111133, 0x000000, 0.6); // слабый «ночной» свет
+        // scene.add(hemi);
+        // const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+        // scene.add(ambientLight);
         // add stationary point lights for debugging
         // FLASHLIGHT_POINTS.forEach((p) => {
         //     const light = new THREE.PointLight(0xffffff, 2, 20);
@@ -1980,7 +1980,7 @@ export function Game({models, sounds, textures, matchId, character}) {
             const playerData = players.get(myPlayerId);
             if (!playerData) return;
             const { mixer, actions } = playerData;
-
+            console.log("actions: ", actions);
 
             lightSword(myPlayerId, 500);
 
@@ -2470,6 +2470,8 @@ export function Game({models, sounds, textures, matchId, character}) {
                                    onEnd = null, // Callback when the animation finishes (only for LoopOnce)
                                }) {
 
+            console.log("action: ", action);
+            console.log("mixer: ", mixer);
             if (!action || !mixer) {
                 console.warn("Invalid action or mixer provided.");
 
@@ -3523,11 +3525,11 @@ export function Game({models, sounds, textures, matchId, character}) {
                 jump: get(map.jump),
                 casting: get(map.casting),
                 castEnd: get(map.castEnd),
-                cast: get('cast'),
+                cast: get(map.cast),
                 dying: get(map.dying),
-                hitReaction: get('hit_reaction'),
-                attack: get('attack'),
-                attack360: get('attack_360'),
+                hitReaction: get(map.hitReaction),
+                attack: get(map.attack),
+                attack360: get(map.attack360),
             };
 
             // Speed up attack animations
@@ -3579,6 +3581,7 @@ export function Game({models, sounds, textures, matchId, character}) {
                 // const idle = mixer.clipAction(animations[2]).play();
                 // const walk = mixer.clipAction(animations[6]);
                 console.log("animations: ", animations);
+                console.log("characterModel: ", characterModel);
                 const actions = buildActions(mixer, animations, characterModel);
 
 
